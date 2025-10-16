b796330c74c16e9aee7762446a57d8155328f7ca
ntrioooo <ntrio23331@gmail.com>
2025-10-16 16:48:44 +0700

    belajar go pakai postgres

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"belajar-go/controllers"
+	"belajar-go/initializers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+}
+
+func main() {
+	r := gin.Default()
+
+	// Define a simple GET endpoint
+	r.POST("/posts", controllers.PostsCreate)
+	r.GET("/posts", controllers.PostsIndex)
+	r.GET("/posts/:id", controllers.PostsShow)
+	r.PUT("/posts/:id", controllers.PostsUpdate)
+	r.DELETE("/posts/:id", controllers.PostsDelete)
+
+	// Start server on port 8080 (default)
+	// Server will listen on 0.0.0.0:8080 (localhost:8080 on Windows)
+	r.Run()
+}
---
+++ migrate/migrate.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"belajar-go/initializers"
+	"belajar-go/models"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+}
+
+func main() {
+	initializers.DB.AutoMigrate(&models.Post{})
+}
